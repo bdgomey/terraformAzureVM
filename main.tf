6d3b735374f40e66dbc37ac5ed9309bd08530ec1
BrianJGomes <105877410+BrianJGomes@users.noreply.github.com>
2022-09-14 13:57:55 -0500

    made adjustments

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 module "resource_group" {
     source = "github.com/bdgomey/terraform_modules_git/resource_group"
-    
+    resource_group_name = var.resource_group_name
+    resource_group_location = var.resource_group_location
 }
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+resource_group_name = "bgomes-RG"
+
+resource_group_location = "eastus"
